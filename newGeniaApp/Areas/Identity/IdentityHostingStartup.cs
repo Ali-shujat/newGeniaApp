d79046dd9516d650f28d4de0045da29a815e22dd
Shujat Ali <shuja4you@hotmail.com>
2020-11-19 16:44:48 +0100

    Add project files.

---
+++ newGeniaApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using newGeniaApp.Data;
+
+[assembly: HostingStartup(typeof(newGeniaApp.Areas.Identity.IdentityHostingStartup))]
+namespace newGeniaApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
